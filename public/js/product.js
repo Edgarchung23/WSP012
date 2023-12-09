c618e4ffa1f51e3ed52a64605e1d552ed58c2ce7
navythk <hoikwan0728@hotmail.com>
2023-12-09 16:33:03 +0800

    updated 9 dec

--- public/js/product.js
+++ public/js/product.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 
     $('.color-choose input').on('click', function() {
-        var headphonesColor = $(this).attr('data-image');
+        const ballColor = $(this).attr('data-image');
   
         $('.active').removeClass('active');
-        $('.left-column img[data-image = ' + headphonesColor + ']').addClass('active');
+        $('.left-column img[data-image = ' + ballColor + ']').addClass('active');
         $(this).addClass('active');
     });
   
@@ -46,4 +46,3 @@ $(document).ready(function() {
   
     window.location.reload();
   }
-  
